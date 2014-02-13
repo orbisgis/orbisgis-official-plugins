23e20325aa7154a2de461041bddb0d92d37289b7
nicolas-f <github@nettrader.fr>
2014-02-13 14:37:51 +0100

    Groovy console for OrbisGIS-h2

---
+++ groovyConsole/src/main/java/org/orbisgis/groovy/GroovyConsoleActions.java
@@ -0,0 +1,17 @@
+package org.orbisgis.groovy;
+
+import org.orbisgis.viewapi.components.actions.ActionFactoryService;
+
+/**
+ * @author Nicolas Fortin
+ */
+public abstract class GroovyConsoleActions implements ActionFactoryService<GroovyConsolePanel> {
+    // Action, MENU IDs
+    public static final String A_EXECUTE = "M_EXECUTE";
+    public static final String A_CLEAR = "M_CLEAR";
+    public static final String A_OPEN = "M_OPEN";
+    public static final String A_SAVE = "M_SAVE";
+    public static final String A_SEARCH = "M_SEARCH";
+    public static final String A_COMMENT = "M_COMMENT";
+    public static final String A_BLOCKCOMMENT = "M_BLOCKCOMMENT";
+}
