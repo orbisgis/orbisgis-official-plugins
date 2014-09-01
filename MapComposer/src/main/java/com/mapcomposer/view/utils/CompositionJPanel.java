559bd49d4db3e439d0072e53d585c7e640acacb5
Palominos Sylvain <sylvain.palominos@eleves.ec-nantes.fr>
2014-09-01 11:51:36 +0200

    Implements the resize of the element by the mouse, Now when an element is moved, it follows the mouse.

--- MapComposer/src/main/java/com/mapcomposer/view/utils/CompositionJPanel.java
+++ MapComposer/src/main/java/com/mapcomposer/view/utils/CompositionJPanel.java
@@ -3,9 +3,11 @@
 import com.mapcomposer.controller.UIController;
 import com.mapcomposer.model.graphicalelement.GraphicalElement;
 import java.awt.BorderLayout;
+import java.awt.Cursor;
 import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
@@ -13,7 +15,7 @@ import javax.swing.border.Border;
 /**
  * This panel extends from JPanel define the action to do when the user click on it.
  */
-public class CompositionJPanel extends JPanel implements MouseListener{
+public class CompositionJPanel extends JPanel implements MouseListener, MouseMotionListener{
     
     private JPanel panel;
     private final GraphicalElement ge;
@@ -22,25 +24,27 @@ public class CompositionJPanel extends JPanel implements MouseListener{
     private int startX;
     private int startY;
     
+    private char moveMod;
+    
     public CompositionJPanel(GraphicalElement ge){
         super(new BorderLayout());
+        moveMod = 0;
         this.ge=ge;
         panel=new JPanel();
         selected = false;
         this.add(panel, BorderLayout.CENTER);
         this.addMouseListener(this);
+        this.addMouseMotionListener(this);
         startX=0;
         startY=0;
     }
     public void setPanel(JPanel panel){
         this.panel = panel;
         this.selected=false;
-        setborders();
-        //panel.revalidate();
-        //panel.repaint();
+        setBorders();
     }
 
-    private void setborders() {
+    private void setBorders() {
        if(selected){
         Border raisedbevel = BorderFactory.createRaisedBevelBorder();
         Border loweredbevel = BorderFactory.createLoweredBevelBorder();
@@ -54,7 +58,7 @@ public class CompositionJPanel extends JPanel implements MouseListener{
     @Override
     public void mouseClicked(MouseEvent me) {
         this.selected=!selected;
-        setborders();
+        setBorders();
         if(selected){
             UIController.getInstance().selectGE(ge);
             panel.setSize((int)this.getSize().getWidth()+8, (int)this.getSize().getHeight()+8);
@@ -73,17 +77,66 @@ public class CompositionJPanel extends JPanel implements MouseListener{
 
     @Override
     public void mousePressed(MouseEvent me) {
-        startX = me.getX();
-        startY = me.getY();
+        startX = me.getLocationOnScreen().x;
+        startY = me.getLocationOnScreen().y;
+        System.out.println("x:"+startX+", y:"+startY);
+        if(me.getX()>0 && me.getX()<5){
+            moveMod=1;
+            this.setCursor(new Cursor(Cursor.E_RESIZE_CURSOR));
+        }
+        else if(me.getX()>ge.getWidth()-5 && me.getX()<ge.getWidth()){
+            moveMod=2;
+            this.setCursor(new Cursor(Cursor.W_RESIZE_CURSOR));
+        }
+        else if(startY>0 && startY<5){
+            moveMod=3;
+            this.setCursor(new Cursor(Cursor.N_RESIZE_CURSOR));
+        }
+        else if(startY>ge.getHeight()-5 && startY<ge.getHeight()){
+            moveMod=4;
+            this.setCursor(new Cursor(Cursor.S_RESIZE_CURSOR));
+        }
+        else{
+            this.setCursor(new Cursor(Cursor.MOVE_CURSOR));
+            moveMod=5;
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent me) {
-        if(!selected){
-            ge.setX(ge.getX()-startX+me.getX());
-            ge.setY(ge.getY()-startY+me.getY());
-            this.panel.setLocation(ge.getX(), ge.getY());
+        switch(moveMod){
+            case 1:
+                ge.setWidth(startX-me.getLocationOnScreen().x+ge.getWidth());
+                ge.setX(ge.getX()-(startX-me.getLocationOnScreen().x));
+                panel.setBounds(ge.getX(), ge.getY(),  ge.getWidth(), ge.getHeight());
+                break;
+            case 2:
+                ge.setWidth(-(startX-me.getLocationOnScreen().x)+ge.getWidth());
+                panel.setBounds(ge.getX(), ge.getY(), ge.getWidth(), ge.getHeight());
+                break;
+            case 3:
+                ge.setHeight(startY-me.getLocationOnScreen().y);
+                ge.setY(ge.getY()-(startY-me.getLocationOnScreen().y));
+                panel.setBounds(ge.getX(), ge.getY(), ge.getWidth(), ge.getHeight());
+                break;
+            case 4:
+                ge.setHeight(-(startY-me.getLocationOnScreen().y));
+                panel.setBounds(ge.getX(), ge.getY(), ge.getWidth(), ge.getHeight());
+                break;
+            case 5:
+                ge.setX(ge.getX()-startX+me.getLocationOnScreen().x);
+                ge.setY(ge.getY()-startY+me.getLocationOnScreen().y);
+                this.panel.setLocation(ge.getX(), ge.getY());
+                break;
         }
+        if(selected){
+            UIController.getInstance().unselectGE(ge);
+            UIController.getInstance().selectGE(ge);
+        }
+        setBorders();
+        moveMod=0;
+        
+        this.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
     }
 
     @Override
@@ -91,4 +144,30 @@ public class CompositionJPanel extends JPanel implements MouseListener{
 
     @Override
     public void mouseExited(MouseEvent me) {}
+
+    @Override
+    public void mouseDragged(MouseEvent me) {
+        panel.setLocation(ge.getX()-(startX-me.getLocationOnScreen().x), ge.getY()-(startY-me.getLocationOnScreen().y));
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent me) {
+        int x = me.getX();
+        int y = me.getY();
+        if(x>0 && x<10){
+            this.setCursor(new Cursor(Cursor.E_RESIZE_CURSOR));
+        }
+        else if(x>ge.getWidth()-10 && x<ge.getWidth()+10){
+            this.setCursor(new Cursor(Cursor.W_RESIZE_CURSOR));
+        }
+        else if(y>0 && y<10){
+            this.setCursor(new Cursor(Cursor.N_RESIZE_CURSOR));
+        }
+        else if(y>ge.getHeight()-10 && y<ge.getHeight()+10){
+            this.setCursor(new Cursor(Cursor.S_RESIZE_CURSOR));
+        }
+        else{
+            this.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        }
+    }
 }
