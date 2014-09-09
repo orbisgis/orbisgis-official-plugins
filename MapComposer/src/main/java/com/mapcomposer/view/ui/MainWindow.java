20ffebf4c9443433f51f1a48066cd9fafc3f2ff4
Palominos Sylvain <sylvain.palominos@eleves.ec-nantes.fr>
2014-09-09 09:46:56 +0200

    Now the plugin is launched in OrbisGIS, in the tools menu.

--- MapComposer/src/main/java/com/mapcomposer/view/ui/MainWindow.java
+++ MapComposer/src/main/java/com/mapcomposer/view/ui/MainWindow.java
@@ -2,20 +2,31 @@ package com.mapcomposer.view.ui;
 
 import com.mapcomposer.controller.UIController;
 import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.beans.EventHandler;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.Action;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import org.orbisgis.view.icons.OrbisGISIcon;
+import org.orbisgis.viewapi.components.actions.DefaultAction;
+import org.orbisgis.viewapi.main.frames.ext.MainFrameAction;
+import static org.orbisgis.viewapi.main.frames.ext.MainFrameAction.MENU_TOOLS;
 //import org.orbisgis.view.icons.OrbisGISIcon;
 
 /**
  * Main window of the map composer. during the developement, the map composer will be a separeted window.
  * Once finished, it probably will become a part of OrbisGIS using the dockable interface.
  */
-public class MainWindow extends JFrame{
+public class MainWindow extends JFrame implements MainFrameAction{
     
     /**Unique instance of the class.*/
     private static MainWindow INSTANCE=null;
+    
+    public static String MENU_MAPCOMPOSER = "MapComposer";
     
     /**Private constructor.*/
     private MainWindow(){
@@ -43,5 +54,23 @@ public class MainWindow extends JFrame{
         if(INSTANCE==null)
             INSTANCE = new MainWindow();
         return INSTANCE;
+    }
+    
+    public void showMapComposer(){
+        this.setVisible(true);
+    }
+
+    @Override
+    public List<Action> createActions(org.orbisgis.viewapi.main.frames.ext.MainWindow target) {
+        List<Action> actions = new ArrayList<Action>();
+        actions.add(new DefaultAction(MENU_MAPCOMPOSER,"Map Composer",
+                new ImageIcon(),
+                EventHandler.create(ActionListener.class,this,"showMapComposer")).setParent(MENU_TOOLS));
+        return actions;
+    }
+
+    @Override
+    public void disposeActions(org.orbisgis.viewapi.main.frames.ext.MainWindow target, List<Action> actions) {
+        this.dispose();
     }
 }
