a07c802d9a0f8d6b1b876a7c386da685715b1f03
Palominos Sylvain <sylvain.palominos@eleves.ec-nantes.fr>
2014-11-10 14:18:40 +0100

    The ColorChooser and ColorRenderer classes now use EventHandler instead of listeners.

--- MapComposer/src/main/java/org/orbisgis/mapcomposer/view/utils/ColorChooser.java
+++ MapComposer/src/main/java/org/orbisgis/mapcomposer/view/utils/ColorChooser.java
@@ -1,53 +1,40 @@
 package org.orbisgis.mapcomposer.view.utils;
 
 import java.awt.Color;
-import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.beans.EventHandler;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
  * Color chooser
  */
-public class ColorChooser extends JFrame implements MouseListener{
-    private JLabel label;
-    private JFrame frame;
-    private JButton button;
+public class ColorChooser extends JFrame{
+    private final JButton label;
+    private final JButton button;
     final JColorChooser jcc;
     
-    public ColorChooser(JLabel label){
+    public ColorChooser(JButton label){
         this.label = label;
         jcc = new JColorChooser(Color.yellow);
-        frame = new JFrame();
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
         panel.add(jcc);
-        button = new JButton("Ok");
-        button.addMouseListener(this);
-        button.setSize(40, 20);
+        this.button = new JButton("Ok");
+        this.button.addMouseListener(EventHandler.create(MouseListener.class, this, "mouseClicked", "source", "mouseClicked"));
+        this.button.setSize(40, 20);
         panel.add(button);
-        frame.add(panel);
-        frame.pack();
+        this.add(panel);
+        this.pack();
     }
 
-    @Override public void mouseClicked(MouseEvent me) {
-        frame.setVisible(true);
-        if(me.getSource()==button){
-            label.setForeground(jcc.getColor());
-            frame.setVisible(false);
+   public void mouseClicked(Object o) {
+        if(o==button){
+            label.setBackground(jcc.getColor());
+            this.setVisible(false);
         }
     }
-
-    @Override public void mousePressed(MouseEvent me) {}
-
-    @Override public void mouseReleased(MouseEvent me) {}
-
-    @Override public void mouseEntered(MouseEvent me) {}
-
-    @Override public void mouseExited(MouseEvent me) {}
-    
 }
