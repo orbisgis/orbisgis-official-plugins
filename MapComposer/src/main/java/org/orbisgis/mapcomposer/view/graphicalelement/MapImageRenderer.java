808e798002ab8c0c03482c5dd55358e1511e68c9
Palominos Sylvain <sylvain.palominos@eleves.ec-nantes.fr>
2014-12-10 16:54:08 +0100

    On loading an map in an MapImage GraphicalElement, it is scaled to the GE size and not crop, Now the MapTransform is using as extent and image size the envelop of the MapContext.

--- MapComposer/src/main/java/org/orbisgis/mapcomposer/view/graphicalelement/MapImageRenderer.java
+++ MapComposer/src/main/java/org/orbisgis/mapcomposer/view/graphicalelement/MapImageRenderer.java
@@ -15,19 +15,27 @@ public class MapImageRenderer extends SimpleGERenderer {
     
     @Override
     public BufferedImage createImageFromGE(GraphicalElement ge) {
+        BufferedImage bi;
         // Draw in a BufferedImage the map image
         if(((MapImage)ge).getImage()!=null){
-        return applyRotationToBufferedImage(((MapImage)ge).getImage(), ge);
+            bi = ((MapImage)ge).getImage();
         }
         else{
             //Return the icon of the MapImage as BufferedImage
             ImageIcon icon = new ImageIcon(MainWindow.class.getResource("add_map.png"));
-            BufferedImage bi = new BufferedImage(icon.getIconWidth(),icon.getIconHeight(),BufferedImage.TYPE_INT_ARGB);
+            bi = new BufferedImage(icon.getIconWidth(),icon.getIconHeight(),BufferedImage.TYPE_INT_ARGB);
             Graphics g = bi.createGraphics();
             icon.paintIcon(null, g, 0,0);
             g.dispose();
-            return applyRotationToBufferedImage(bi, ge);
         }
+
+        //Scale the bufferedImage to the GraphicalElement size
+        java.awt.Image image = bi.getScaledInstance(ge.getWidth(), ge.getHeight(), java.awt.Image.SCALE_FAST);
+        BufferedImage resize = new BufferedImage(ge.getWidth(), ge.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics graph = resize.createGraphics();
+        graph.drawImage(image, 0, 0, null);
+        graph.dispose();
+
+        return applyRotationToBufferedImage(resize, ge);
     }
-    
 }
