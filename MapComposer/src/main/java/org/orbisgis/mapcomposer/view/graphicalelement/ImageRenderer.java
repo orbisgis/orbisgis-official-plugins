d43bb68f9c7ece23735439b85fef64d2f682ce25
Palominos Sylvain <sylvain.palominos@eleves.ec-nantes.fr>
2014-12-10 16:08:51 +0100

    On loading an image in an Image GraphicalElement, it is scaled to the GE size and not crop.

--- MapComposer/src/main/java/org/orbisgis/mapcomposer/view/graphicalelement/ImageRenderer.java
+++ MapComposer/src/main/java/org/orbisgis/mapcomposer/view/graphicalelement/ImageRenderer.java
@@ -6,10 +6,6 @@ import org.orbisgis.mapcomposer.view.ui.MainWindow;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
 /**
@@ -19,24 +15,27 @@ public class ImageRenderer extends SimpleGERenderer {
 
     @Override
     public BufferedImage createImageFromGE(GraphicalElement ge) {
-        // Draw in a BufferedImage the image file
+        //Get the image to draw
         File file = new File(((Image)ge).getPath());
-        if(file.exists() && file.isFile()) {
-            ImageIcon icon = new ImageIcon(((Image)ge).getPath());
-            BufferedImage bi = new BufferedImage(icon.getIconWidth(),icon.getIconHeight(),BufferedImage.TYPE_INT_ARGB);
-            Graphics g = bi.createGraphics();
-            icon.paintIcon(null, g, 0,0);
-            g.dispose();
-            return applyRotationToBufferedImage(bi, ge);
-        }
-        else{
-            //Return the icon of the Image as BufferedImage
-            ImageIcon icon = new ImageIcon(MainWindow.class.getResource("add_picture.png"));
-            BufferedImage bi = new BufferedImage(icon.getIconWidth(),icon.getIconHeight(),BufferedImage.TYPE_INT_ARGB);
-            Graphics g = bi.createGraphics();
-            icon.paintIcon(null, g, 0,0);
-            g.dispose();
-            return applyRotationToBufferedImage(bi, ge);
-        }
+        ImageIcon icon;
+        if(file.exists() && file.isFile())
+            icon = new ImageIcon(((Image)ge).getPath());
+        else
+            icon = new ImageIcon(MainWindow.class.getResource("add_picture.png"));
+
+        //Get the bufferedImage from the image
+        BufferedImage bi = new BufferedImage(icon.getIconWidth(),icon.getIconHeight(),BufferedImage.TYPE_INT_ARGB);
+        Graphics g = bi.createGraphics();
+        icon.paintIcon(null, g, 0,0);
+        g.dispose();
+
+        //Scale the bufferedImage to the GraphicalElement size
+        java.awt.Image image = bi.getScaledInstance(ge.getWidth(), ge.getHeight(), java.awt.Image.SCALE_FAST);
+        BufferedImage resize = new BufferedImage(ge.getWidth(), ge.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        Graphics graph = resize.createGraphics();
+        graph.drawImage(image, 0, 0, null);
+        graph.dispose();
+
+        return applyRotationToBufferedImage(resize, ge);
     }
 }
